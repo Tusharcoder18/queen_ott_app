2ed1f846e38c0ef4dd0ba9323feaab4fdfc62242
Tusharcoder18 <ghostridertushar@gmail.com>
2021-03-27 17:03:55 +0530

    SharedPreferences for App Settings

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -5,6 +5,7 @@ import 'package:queen_ott_app/screens/content_creator_screen.dart';
 import 'package:queen_ott_app/screens/user_home_screen.dart';
 import 'package:queen_ott_app/services/authentication_service.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsPage extends StatefulWidget {
   final bool isCreator;
@@ -15,9 +16,35 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  bool explicit = false;
-  bool notificationsEnabled = true;
-  bool globeNews = false;
+  bool explicit;
+  bool notificationsEnabled;
+  bool globeNews;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getPreferences().whenComplete(() {
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
+
+  Future<void> getPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    explicit = prefs.getBool('explicit') ?? false;
+    notificationsEnabled = prefs.getBool('notificationsEnabled') ?? false;
+    globeNews = prefs.getBool('globeNews') ?? false;
+  }
+
+  updatePreferences(String setting, bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(setting, value);
+    setState(() {
+      print('Settings saved successfully');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,112 +60,121 @@ class _SettingsPageState extends State<SettingsPage> {
           ),
         ),
       ),
-      body: Container(
-        child: SettingsList(
-          sections: [
-            CustomSection(
-              child: SettingsTile(
-                title: 'User Name',
-                leading: Icon(FontAwesomeIcons.user),
-                subtitle: 'Update Profile',
-                trailing: Icon(Icons.edit),
-                onPressed: (context) {},
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Container(
+              child: SettingsList(
+                sections: [
+                  CustomSection(
+                    child: SettingsTile(
+                      title: 'User Name',
+                      leading: Icon(FontAwesomeIcons.user),
+                      subtitle: 'Update Profile',
+                      trailing: Icon(Icons.edit),
+                      onPressed: (context) {},
+                    ),
+                  ),
+                  SettingsSection(
+                    title: 'Account',
+                    tiles: [
+                      SettingsTile(
+                        title: widget.isCreator
+                            ? 'Switch to Consumer'
+                            : 'Switch to Creator',
+                        leading: Icon(FontAwesomeIcons.creativeCommons),
+                        onPressed: (context) {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            if (widget.isCreator) {
+                              return HomeScreen();
+                            } else {
+                              return ContentCreatorScreen();
+                            }
+                          }));
+                        },
+                      ),
+                      SettingsTile(title: 'Email', leading: Icon(Icons.email)),
+                    ],
+                  ),
+                  SettingsSection(
+                    title: 'Common',
+                    tiles: [
+                      SettingsTile(
+                        title: 'Language',
+                        subtitle: 'English',
+                        leading: Icon(Icons.language),
+                        onPressed: (context) {},
+                      ),
+                    ],
+                  ),
+                  SettingsSection(
+                    title: 'Security',
+                    tiles: [
+                      SettingsTile(
+                        title: 'Password',
+                        leading: Icon(Icons.lock),
+                        onPressed: (context) {},
+                      ),
+                      SettingsTile.switchTile(
+                        title: 'Allow Notifications',
+                        leading: Icon(Icons.notifications_active),
+                        switchValue: notificationsEnabled,
+                        onToggle: (value) {
+                          setState(() {
+                            notificationsEnabled = value;
+                            updatePreferences('notificationsEnabled', value);
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  SettingsSection(
+                    title: 'Misc',
+                    tiles: [
+                      SettingsTile.switchTile(
+                        title: 'Explicit',
+                        leading: Icon(Icons.explicit),
+                        switchValue: explicit,
+                        onToggle: (bool value) {
+                          setState(() {
+                            explicit = value;
+                            updatePreferences('explicit', value);
+                          });
+                        },
+                      ),
+                      SettingsTile.switchTile(
+                        title: 'The Globe News Channel',
+                        leading: Icon(Icons.new_releases_sharp),
+                        switchValue: globeNews,
+                        onToggle: (bool value) {
+                          setState(() {
+                            globeNews = value;
+                            updatePreferences('globeNews', value);
+                          });
+                        },
+                      ),
+                      SettingsTile(
+                          title: 'Legal', leading: Icon(Icons.description)),
+                      SettingsTile(
+                          title: 'Terms of Services',
+                          leading: Icon(Icons.description_outlined)),
+                      SettingsTile(
+                        title: 'Sign out',
+                        leading: Icon(Icons.exit_to_app),
+                        onPressed: (context) {
+                          Provider.of<AuthenticationService>(context,
+                                  listen: false)
+                              .signOutFromAll();
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            SettingsSection(
-              title: 'Account',
-              tiles: [
-                SettingsTile(
-                  title: widget.isCreator
-                      ? 'Switch to Consumer'
-                      : 'Switch to Creator',
-                  leading: Icon(FontAwesomeIcons.creativeCommons),
-                  onPressed: (context) {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      if (widget.isCreator) {
-                        return HomeScreen();
-                      } else {
-                        return ContentCreatorScreen();
-                      }
-                    }));
-                  },
-                ),
-                SettingsTile(title: 'Email', leading: Icon(Icons.email)),
-              ],
-            ),
-            SettingsSection(
-              title: 'Common',
-              tiles: [
-                SettingsTile(
-                  title: 'Language',
-                  subtitle: 'English',
-                  leading: Icon(Icons.language),
-                  onPressed: (context) {},
-                ),
-              ],
-            ),
-            SettingsSection(
-              title: 'Security',
-              tiles: [
-                SettingsTile(
-                  title: 'Password',
-                  leading: Icon(Icons.lock),
-                  onPressed: (context) {},
-                ),
-                SettingsTile.switchTile(
-                  title: 'Allow Notifications',
-                  leading: Icon(Icons.notifications_active),
-                  switchValue: notificationsEnabled,
-                  onToggle: (value) {
-                    setState(() {
-                      notificationsEnabled = value;
-                    });
-                  },
-                ),
-              ],
-            ),
-            SettingsSection(
-              title: 'Misc',
-              tiles: [
-                SettingsTile.switchTile(
-                  title: 'Explicit',
-                  leading: Icon(Icons.explicit),
-                  switchValue: explicit,
-                  onToggle: (bool value) {
-                    setState(() {
-                      explicit = value;
-                    });
-                  },
-                ),
-                SettingsTile.switchTile(
-                  title: 'The Globe News Channel',
-                  leading: Icon(Icons.new_releases_sharp),
-                  switchValue: globeNews,
-                  onToggle: (bool value) {
-                    setState(() {
-                      globeNews = value;
-                    });
-                  },
-                ),
-                SettingsTile(title: 'Legal', leading: Icon(Icons.description)),
-                SettingsTile(
-                    title: 'Terms of Services',
-                    leading: Icon(Icons.description_outlined)),
-                SettingsTile(
-                  title: 'Sign out',
-                  leading: Icon(Icons.exit_to_app),
-                  onPressed: (context) {
-                    Provider.of<AuthenticationService>(context, listen: false)
-                        .signOutFromAll();
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
